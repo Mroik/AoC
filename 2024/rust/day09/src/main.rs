aa2d2363aebc1f69cb60dc48525bc47766cf32a7
Mroik <mroik@delayed.space>
2024-12-09 15:30:06 +0100

    Day09 2024

---
+++ 2024/rust/day09/src/main.rs
@@ -0,0 +1,136 @@
+use std::fs::read_to_string;
+
+#[derive(Copy, Clone)]
+enum Block {
+    Empty,
+    Filled(u64),
+}
+
+fn part1() {
+    let mut memory: Vec<Block> = Vec::new();
+    let mut block_id = 0;
+    read_to_string("input")
+        .unwrap()
+        .trim()
+        .chars()
+        .enumerate()
+        .for_each(|(i, size)| {
+            let data = if i % 2 == 1 {
+                Block::Empty
+            } else {
+                let d = Block::Filled(block_id);
+                block_id += 1;
+                d
+            };
+            let size = size as u8 - '0' as u8;
+            memory.extend((0..size as usize).map(|_| data));
+        });
+    let mut left = 0;
+    let mut right = memory.len() - 1;
+    while left < right {
+        match (memory.get(left).unwrap(), memory.get(right).unwrap()) {
+            (Block::Empty, Block::Empty) => right -= 1,
+            (Block::Empty, Block::Filled(_)) => memory.swap(left, right),
+            (Block::Filled(_), Block::Empty) => {
+                left += 1;
+                right -= 1;
+            }
+            (Block::Filled(_), Block::Filled(_)) => left += 1,
+        }
+    }
+
+    let ris: u64 = memory
+        .iter()
+        .enumerate()
+        .map(|(pos, block)| match block {
+            Block::Empty => 0,
+            Block::Filled(id) => id * pos as u64,
+        })
+        .sum();
+    println!("{}", ris);
+}
+
+#[derive(Clone, Copy)]
+enum Block2 {
+    Empty(usize),
+    Filled(u64, usize),
+}
+
+fn part2() {
+    let mut memory: Vec<Block2> = Vec::new();
+    let mut block_id = 0;
+    read_to_string("input")
+        .unwrap()
+        .trim()
+        .chars()
+        .enumerate()
+        .for_each(|(i, size)| {
+            let data = if i % 2 == 1 {
+                Block2::Empty((size as u8 - '0' as u8) as usize)
+            } else {
+                let d = Block2::Filled(block_id, (size as u8 - '0' as u8) as usize);
+                block_id += 1;
+                d
+            };
+            memory.push(data);
+        });
+    memory.retain(|b| match b {
+        Block2::Empty(0) => false,
+        _ => true,
+    });
+
+    let mut cur = memory.len() - 1;
+    loop {
+        for i in 0..memory.len() {
+            if i >= cur {
+                break;
+            }
+            match (
+                memory.get(i).copied().unwrap(),
+                memory.get(cur).copied().unwrap(),
+            ) {
+                (_, Block2::Empty(_)) => (),
+                (Block2::Filled(_, _), _) => (),
+                (Block2::Empty(e_n), Block2::Filled(_, f_n)) => {
+                    if e_n >= f_n {
+                        std::mem::swap(&mut Block2::Empty(f_n), memory.get_mut(i).unwrap());
+                        memory.swap(cur, i);
+                        let remain = e_n - f_n;
+                        if remain > 0 {
+                            memory.insert(i + 1, Block2::Empty(remain));
+                            cur += 1;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        if cur == 0 {
+            break;
+        }
+        cur -= 1;
+    }
+
+    let mut mem = Vec::new();
+    memory.iter().for_each(|b| {
+        let (v, ss) = match b {
+            Block2::Empty(s) => (Block::Empty, s),
+            Block2::Filled(a, s) => (Block::Filled(*a), s),
+        };
+        mem.extend((0..*ss).map(|_| v));
+    });
+    let ris: u64 = mem
+        .iter()
+        .enumerate()
+        .map(|(pos, block)| match block {
+            Block::Empty => 0,
+            Block::Filled(id) => id * pos as u64,
+        })
+        .sum();
+    println!("{}", ris);
+}
+
+fn main() {
+    part1();
+    part2();
+}
