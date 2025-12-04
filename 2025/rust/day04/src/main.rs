48dbb610cab3a3d7276ec91af08d247ca82f5b58
Mroik <mroik@delayed.space>
2025-12-04 15:56:32 +0100

    Less comparisons

--- 2025/rust/day04/src/main.rs
+++ 2025/rust/day04/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::read_to_string;
+use std::{collections::VecDeque, fs::read_to_string};
 
 fn part1() {
     let map: Vec<Vec<bool>> = read_to_string("input")
@@ -71,60 +71,69 @@ fn part2() {
                 .collect()
         })
         .collect();
-
-    let mut ris = 0;
-    let mut removed;
-
-    loop {
-        removed = false;
-        for y in 0..map.len() {
-            for x in 0..map.len() {
-                if !map[y][x] {
-                    continue;
-                }
-
-                let mut adj = 0;
-                if x != 0 && y != 0 && map[y - 1][x - 1] {
-                    adj += 1;
-                }
-
-                if x != 0 && map[y][x - 1] {
-                    adj += 1;
-                }
-
-                if x != 0 && y < map.len() - 1 && map[y + 1][x - 1] {
-                    adj += 1;
-                }
-
-                if y != 0 && map[y - 1][x] {
-                    adj += 1;
-                }
-
-                if y < map.len() - 1 && map[y + 1][x] {
-                    adj += 1;
-                }
-
-                if x < map.len() - 1 && y != 0 && map[y - 1][x + 1] {
-                    adj += 1;
-                }
-
-                if x < map.len() - 1 && map[y][x + 1] {
-                    adj += 1;
-                }
-
-                if x < map.len() - 1 && y < map.len() - 1 && map[y + 1][x + 1] {
-                    adj += 1;
-                }
-
-                if adj < 4 {
-                    map[y][x] = false;
-                    removed = true;
-                    ris += 1;
-                }
+    let mut rolls = VecDeque::new();
+    for y in 0..map.len() {
+        for x in 0..map.len() {
+            if map[y][x] {
+                rolls.push_back((x, y));
             }
         }
+    }
 
-        if !removed {
+    let mut ris = 0;
+    let mut prev;
+
+    loop {
+        prev = rolls.len();
+        for _ in 0..prev {
+            let (x, y) = rolls.pop_front().unwrap();
+            if !map[y][x] {
+                continue;
+            }
+
+            let mut adj = 0;
+            if x != 0 && y != 0 && map[y - 1][x - 1] {
+                adj += 1;
+            }
+
+            if x != 0 && map[y][x - 1] {
+                adj += 1;
+            }
+
+            if x != 0 && y < map.len() - 1 && map[y + 1][x - 1] {
+                adj += 1;
+            }
+
+            if y != 0 && map[y - 1][x] {
+                adj += 1;
+            }
+
+            if y < map.len() - 1 && map[y + 1][x] {
+                adj += 1;
+            }
+
+            if x < map.len() - 1 && y != 0 && map[y - 1][x + 1] {
+                adj += 1;
+            }
+
+            if x < map.len() - 1 && map[y][x + 1] {
+                adj += 1;
+            }
+
+            if x < map.len() - 1 && y < map.len() - 1 && map[y + 1][x + 1] {
+                adj += 1;
+            }
+
+            if adj < 4 {
+                map[y][x] = false;
+                ris += 1;
+                continue;
+            }
+
+            rolls.push_back((x, y));
+        }
+
+        if rolls.len() == prev {
             break;
         }
     }
