26f8875ff921a93dc8494a72004c8bfe20e75d6b
Mroik <mroik@delayed.space>
2025-12-04 20:50:31 +0100

    Improve performance 'cause challenged on big input
    Would've never done this if someone never asked about my runtime on huge
    inputs

--- 2025/rust/day04/src/main.rs
+++ 2025/rust/day04/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::VecDeque, fs::read_to_string};
+use std::{collections::BTreeSet, fs::read_to_string};
 
 fn part1() {
     let map: Vec<Vec<bool>> = read_to_string("input")
@@ -11,9 +11,11 @@ fn part1() {
                 .collect()
         })
         .collect();
-    let ris: usize = (0..map.len())
+    let height = map.len();
+    let width = map[0].len();
+    let ris: usize = (0..height)
         .map(|y| {
-            (0..map.len())
+            (0..width)
                 .filter(|&x| {
                     if !map[y][x] {
                         return false;
@@ -28,7 +30,7 @@ fn part1() {
                         adj += 1;
                     }
 
-                    if x != 0 && y < map.len() - 1 && map[y + 1][x - 1] {
+                    if x != 0 && y < height - 1 && map[y + 1][x - 1] {
                         adj += 1;
                     }
 
@@ -36,19 +38,19 @@ fn part1() {
                         adj += 1;
                     }
 
-                    if y < map.len() - 1 && map[y + 1][x] {
+                    if y < height - 1 && map[y + 1][x] {
                         adj += 1;
                     }
 
-                    if x < map.len() - 1 && y != 0 && map[y - 1][x + 1] {
+                    if x < width - 1 && y != 0 && map[y - 1][x + 1] {
                         adj += 1;
                     }
 
-                    if x < map.len() - 1 && map[y][x + 1] {
+                    if x < width - 1 && map[y][x + 1] {
                         adj += 1;
                     }
 
-                    if x < map.len() - 1 && y < map.len() - 1 && map[y + 1][x + 1] {
+                    if x < width - 1 && y < height - 1 && map[y + 1][x + 1] {
                         adj += 1;
                     }
 
@@ -71,70 +73,76 @@ fn part2() {
                 .collect()
         })
         .collect();
-    let mut rolls = VecDeque::new();
-    for y in 0..map.len() {
-        for x in 0..map.len() {
+    let height = map.len();
+    let width = map[0].len();
+    let mut rolls = BTreeSet::new();
+    for y in 0..height {
+        for x in 0..width {
             if map[y][x] {
-                rolls.push_back((x, y));
+                rolls.insert((x, y));
             }
         }
     }
 
-    let mut ris = 0;
-    let mut prev;
+    let mut ris: u64 = 0;
 
-    loop {
-        prev = rolls.len();
-        for _ in 0..prev {
-            let (x, y) = rolls.pop_front().unwrap();
-            if !map[y][x] {
-                continue;
-            }
-
-            let mut adj = 0;
-            if x != 0 && y != 0 && map[y - 1][x - 1] {
-                adj += 1;
-            }
-
-            if x != 0 && map[y][x - 1] {
-                adj += 1;
-            }
-
-            if x != 0 && y < map.len() - 1 && map[y + 1][x - 1] {
-                adj += 1;
-            }
-
-            if y != 0 && map[y - 1][x] {
-                adj += 1;
-            }
-
-            if y < map.len() - 1 && map[y + 1][x] {
-                adj += 1;
-            }
-
-            if x < map.len() - 1 && y != 0 && map[y - 1][x + 1] {
-                adj += 1;
-            }
-
-            if x < map.len() - 1 && map[y][x + 1] {
-                adj += 1;
-            }
-
-            if x < map.len() - 1 && y < map.len() - 1 && map[y + 1][x + 1] {
-                adj += 1;
-            }
-
-            if adj < 4 {
-                map[y][x] = false;
-                ris += 1;
-                continue;
-            }
-
-            rolls.push_back((x, y));
+    while !rolls.is_empty() {
+        let (x, y) = rolls.iter().next().copied().unwrap();
+        rolls.remove(&(x, y));
+        if !map[y][x] {
+            continue;
         }
 
-        if rolls.len() == prev {
-            break;
+        let mut to_add = Vec::new();
+
+        let mut adj = 0;
+        if x != 0 && y != 0 && map[y - 1][x - 1] {
+            to_add.push((x - 1, y - 1));
+            adj += 1;
+        }
+
+        if x != 0 && map[y][x - 1] {
+            to_add.push((x - 1, y));
+            adj += 1;
+        }
+
+        if x != 0 && y < height - 1 && map[y + 1][x - 1] {
+            to_add.push((x - 1, y + 1));
+            adj += 1;
+        }
+
+        if y != 0 && map[y - 1][x] {
+            to_add.push((x, y - 1));
+            adj += 1;
+        }
+
+        if y < height - 1 && map[y + 1][x] {
+            to_add.push((x, y + 1));
+            adj += 1;
+        }
+
+        if x < width - 1 && y != 0 && map[y - 1][x + 1] {
+            to_add.push((x + 1, y - 1));
+            adj += 1;
+        }
+
+        if x < width - 1 && map[y][x + 1] {
+            to_add.push((x + 1, y));
+            adj += 1;
+        }
+
+        if x < width - 1 && y < height - 1 && map[y + 1][x + 1] {
+            to_add.push((x + 1, y + 1));
+            adj += 1;
+        }
+
+        if adj < 4 {
+            map[y][x] = false;
+            to_add.iter().for_each(|&v| {
+                rolls.insert(v);
+            });
+            ris += 1;
+            continue;
         }
     }
     println!("{}", ris);
